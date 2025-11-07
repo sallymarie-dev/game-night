c8a191c010df7d13520057e5e7c084780852529d
isaacwatson30 <isaacwatson30@gmail.com>
2025-11-06 20:24:52 -0600

    added SplashPage.jsx

---
+++ src/components/SplashPage.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import supabase from "../utils/supabase";
+import "../App.css";
+
+export default function SplashPage(){
+
+
+
+
+
+
+return (
+    <>
+    <div>
+        <h1>Welcome to Family Game Night</h1>
+        <form onSubmit={""}>
+            <label htmlFor=""></label>
+        </form>
+    </div>
+    </>
+)
+}
+
+
